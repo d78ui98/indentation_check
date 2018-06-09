30c0fd0fc7546b73516c7e6e20e67434b6eeb81c
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-09 13:29:22 +0530

    hope indentation is as it is

--- client.sh
+++ client.sh
@@ -27,27 +27,30 @@ OPTIONS:
 
 END
 }
-create_client(){
 
+create_client(){
+	
     echo -n "Enter any name for LTSP client : "
     read clientname
-    echo "Using ${INTERFACE}.." 
+    echo "Using ${INTERFACE}.."	
     VBoxManage createvm --name "${clientname}" --register
     cd "$HOME/VirtualBox VMs/${clientname}"
-    VBoxManage modifyvm "${clientname}"\
-         --memory 1024\
-         --acpi on\
-         --boot1 net\
-         --boot2 none\
-         --boot3 none\
-         --boot4 none\
-         --nic1 bridged\
-        --bridgeadapter1 eno1\
-         --nicpromisc1 allow-all
-    VBoxManage createhd --filename "$HOME/VirtualBox VMs/${clientname}/${clientname}.vdi" --size 1
+    VBoxManage modifyvm "${clientname}" \
+        --memory 1024 \
+        --acpi on \
+        --boot1 net \
+        --boot2 none \
+        --boot3 none \
+        --boot4 none \
+        --nic1 bridged \
+        --bridgeadapter1 $INTERFACE \
+        --nicpromisc1 allow-all 
+    VBoxManage createhd --filename \
+        "$HOME/VirtualBox VMs/${clientname}/${clientname}.vdi" --size 1
     VBoxManage storagectl "${clientname}" --name "IDE Controller" --add ide
     VBoxManage startvm "${clientname}"
 }
+
 
 if [ "$1" == "" ]; then
     create_client
